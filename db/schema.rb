c3cf959ca8412f1d871ad39ff762b349868d8041
jioherry <jioherry@gmail.com>
2017-04-21 15:23:20 +0800

    add posts column, name:country_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170421064321) do
+ActiveRecord::Schema.define(version: 20170421071603) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20170421064321) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "country_id"
   end
 
   create_table "users", force: :cascade do |t|
