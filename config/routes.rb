3092ffabc2244484efeb30863cdff5e57e24ba80
jimmy <jimmyyang5296@gmail.com>
2017-04-28 10:10:49 +0800

    首頁可以登出

--- config/routes.rb
+++ config/routes.rb
@@ -3,22 +3,18 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
-<<<<<<< HEAD
-=======
+
   devise_for :users, controllers: { registrations: "registrations"}
     get 'posts/signup', to: 'posts#signup'
 
->>>>>>> front_end
-
-  devise_for :users
-
-<<<<<<< HEAD
   resources :posts do
     collection do
       get :service
       get :aboutus
       get :work_case
-=======
+    end
+  end
+
 
 
   resources :posts do
@@ -31,12 +27,9 @@ Rails.application.routes.draw do
         get :info
         get :aboutus
       end
->>>>>>> e3667cebbe37fad44a0916858f542f283cfa34ae
     end
 
-
   resources :articles
-
   root 'posts#index'
 
 end
