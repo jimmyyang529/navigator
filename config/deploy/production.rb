401f2a865cfe1949b8a0b9877d0cbd8cf54b0ca3
jioherry <jioherry@gmail.com>
2017-05-02 11:18:26 +0800

    部署測試

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,11 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "172.104.42.225", user: "ray", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-server '172.104.42.225', user: 'ray', roles: %w{app db web}, my_property: :my_value
 
 # role-based syntax
 # ==================
