61cc5917c2a06116fc2b4df74c4c178848f99f48
jimmy <jimmyyang5296@gmail.com>
2017-05-03 16:13:26 +0800

    調整圖片寬高400x400

--- app/uploaders/posts_uploader.rb
+++ app/uploaders/posts_uploader.rb
@@ -32,19 +32,20 @@ class PostsUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  process resize_to_fit: [800, 800] #圖片上傳後，自動切成你要的size
+  process resize_to_fit: [800, 800]
+  #圖片上傳後，自動切成你要的size
 
   #設同時切其他size的版本-thumb
   # Create different versions of your uploaded files:
   version :thumb do
    process resize_to_fit: [400, 400]
   end
-
-  Add a white list of extensions which are allowed to be uploaded.
-  For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  #
+  # Add a white list of extensions which are allowed to be uploaded.
+  # For images you might use something like this:
+  # def extension_whitelist
+  #   %w(jpg jpeg gif png)
+  # end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
